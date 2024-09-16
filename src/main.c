eaed0d8054899e6ee9215a77cd4cbe423450269e
rurain7763 <rurain7763@gmail.com>
2024-09-16 11:56:00 +0900

    face_t, triangle_t struct 추가 및 기존 큐브에 적용

--- src/main.c
+++ src/main.c
@@ -1,9 +1,8 @@
 #include "display.h"
 #include "vector.h"
+#include "mesh.h"
 
-#define N_POINTS (9 * 9 * 9)
-vec3_t cube[N_POINTS];
-vec2_t projected_points[N_POINTS];
+triangle_t triangles_to_render[N_MESH_FACES];
 vec3_t cube_roation = { .x = 0, .y = 0, .z = 0};
 
 vec3_t camera_position = { .x = 0, .y = 0, .z = -5};
@@ -38,16 +37,6 @@ void setup() {
         window_width,
         window_height
     );
-
-    int idx = 0;
-    for(float x = -1; x <= 1; x += 0.25) {
-        for(float y = -1; y <= 1; y += 0.25) {
-            for(float z = -1; z <= 1; z += 0.25) {
-                vec3_t p = { .x = x, .y = y, .z = z};
-                cube[idx++] = p;
-            }
-        }
-    }
 }
 
 void process_input() {
@@ -84,32 +73,47 @@ void update() {
     cube_roation.z += 0.01;
     cube_roation.x += 0.01;
 
-    for(int i = 0; i < N_POINTS; i++) {
-        vec3_t point = cube[i];
+    for(int i = 0; i < N_MESH_FACES; i++) {
+        face_t mesh_face = mesh_faces[i];
 
-        vec3_t transformed_point = vec3_roate_x(point, cube_roation.x);
-        transformed_point = vec3_roate_y(transformed_point, cube_roation.y);
-        transformed_point = vec3_roate_z(transformed_point, cube_roation.z);
+        vec3_t vertices[3];
+        vertices[0] = mesh_vertices[mesh_face.a - 1];
+        vertices[1] = mesh_vertices[mesh_face.b - 1];
+        vertices[2] = mesh_vertices[mesh_face.c - 1];
 
-        transformed_point.z -= camera_position.z;
+        triangle_t triangle;
+        for(int j = 0; j < 3; j++) {
+            vec3_t transformed_vertex = vec3_roate_x(vertices[j], cube_roation.x);
+            transformed_vertex = vec3_roate_y(transformed_vertex, cube_roation.y);
+            transformed_vertex = vec3_roate_z(transformed_vertex, cube_roation.z);
+            
+            transformed_vertex.z -= camera_position.z;
 
-        vec2_t projected_point = project(transformed_point);
-        projected_points[i] = projected_point;
+            vec2_t projected_point = project(transformed_vertex);
+            projected_point.x += window_width / 2;
+            projected_point.y += window_height / 2;
+
+            triangle.points[j] = projected_point;
+        }
+
+        triangles_to_render[i] = triangle;
     }
 }
 
 void render() {
     draw_grid(0xFF00FFFF, 10, 10);
 
-    for(int i = 0; i < N_POINTS; i++) {
-        vec2_t projected_point = projected_points[i];
-        draw_fill_rect(
-            projected_point.x + (window_width / 2), 
-            projected_point.y + (window_height / 2), 
-            4,
-            4,
-            0xFFFFFF00
-        );
+    for(int i = 0; i < N_MESH_FACES; i++) {
+        triangle_t triangle = triangles_to_render[i];
+        for(int j = 0; j < 3; j++) {
+            draw_fill_rect(
+                triangle.points[j].x, 
+                triangle.points[j].y, 
+                4,
+                4,
+                0xFFFFFF00
+            );
+        }
     }
 
     render_color_buffer();
